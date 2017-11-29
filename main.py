3d98666998e27a9705e8e20dc37c21a47fcbdc48
llayman <laymanl@uncw.edu>
2017-11-29 13:12:37 -0500

    Added MongoDB and restructured a few things

--- main.py
+++ main.py
@@ -4,19 +4,12 @@ from __future__ import print_function
 import tweepy
 import json
 import MySQLdb 
+from pymongo import MongoClient
 import time
-import logging
 from dateutil import parser
 from ConfigParser import SafeConfigParser
 import os
 
-LOG_DIR = 'log'
-
-if not os.path.exists(LOG_DIR):
-    os.makedirs(LOG_DIR)
-
-
-COUNT = 0
 
 WORDS = ['#bigdata', '#AI', '#datascience', '#machinelearning', '#ml', '#iot']
 
@@ -33,53 +26,64 @@ USER = conf.get('mysql', 'user')
 PASSWD = conf.get('mysql', 'password')
 DATABASE = conf.get('mysql', 'database')
 
+MONGO_HOST = conf.get('mongo', 'host')
+MONGO_USER = conf.get('mongo', 'user')
+MONGO_PASSWD = conf.get('mongo', 'password')
+MONGO_DATABASE = conf.get('mongo', 'database')
+MONGO_COLLECTION = conf.get('mongo', 'collection')
+
 # This function takes the 'created_at', 'text', 'screen_name' and 'tweet_id' and stores it
 # into a MySQL database
-def store_data(created_at, text, screen_name, tweet_id):
-    db=MySQLdb.connect(host=HOST, user=USER, passwd=PASSWD, db=DATABASE, charset="utf8mb4")
-    cursor = db.cursor()
-    insert_query = "INSERT INTO twitter (tweet_id, screen_name, created_at, text) VALUES (%s, %s, %s, %s)"
-    cursor.execute(insert_query, (tweet_id, screen_name, created_at.strftime('%Y-%m-%d %H:%M:%S'), text))
-    db.commit()
-    cursor.close()
-    db.close()
-
-    global COUNT
-    COUNT = COUNT + 1
-    if not COUNT % 100:
-        print(COUNT, "tweets collected.")
-    return
 
 class StreamListener(tweepy.StreamListener):    
     #This is a class provided by tweepy to access the Twitter Streaming API. 
 
+    insert_query = "INSERT INTO twitter (tweet_id, screen_name, created_at, text) VALUES (%s, %s, %s, %s)"
+
+
+    def __init__(self, api):
+        mongo_client = MongoClient(MONGO_HOST)
+        mongo_db = mongo_client[MONGO_DATABASE]
+        mongo_db.authenticate(MONGO_USER, MONGO_PASSWD)
+        self.coll = mongo_db[MONGO_COLLECTION]
+        self.count = self.coll.count()
+        print(self.count, "tweets currently in database.")
+
+
     def on_connect(self):
         # Called initially to connect to the Streaming API
         print("You are now connected to the streaming API.")
- 
+
     def on_error(self, status_code):
         # On error - if an error occurs, display the error / status code
         print('An Error has occured: ' + repr(status_code))
         return False
- 
+
     def on_data(self, data):
         #This is the meat of the script...it connects to your mongoDB and stores the tweet
         try:
            # Decode the JSON from Twitter
             datajson = json.loads(data)
-            
+
             #grab the wanted data from the Tweet
             text = datajson['text']
             screen_name = datajson['user']['screen_name']
             tweet_id = datajson['id']
             created_at = parser.parse(datajson['created_at'])
 
-            #print out a message to the screen that we have collected a tweet
-            #print("Tweet collected at " + str(created_at))
-            
             #insert the data into the MySQL database
-            store_data(created_at, text, screen_name, tweet_id)
-        
+            conn=MySQLdb.connect(host=HOST, user=USER, passwd=PASSWD, db=DATABASE, charset="utf8mb4")
+            with conn as cursor:
+                cursor.execute(self.insert_query, (tweet_id, screen_name, created_at.strftime('%Y-%m-%d %H:%M:%S'), text))
+            conn.close()
+
+            #insert into MongoDB
+            self.coll.insert(datajson)
+
+            self.count = self.count + 1
+            if self.count % 1000 == 0:
+                print(self.count, "tweets collected.")
+
         except Exception as e:
            print(e)
 
